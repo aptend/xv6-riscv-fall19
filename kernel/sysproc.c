5801ea0d447fe6b43cffdd827f34b0898f9fe0e6
aptend <crescentwhale@hotmail.com>
2020-06-01 02:12:31 -0700

    delete growproc in sys_sbrk

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -46,9 +46,9 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  struct proc *p = myproc();
+  addr = p->sz;
+  p->sz += n;
   return addr;
 }
 
